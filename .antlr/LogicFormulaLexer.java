0632db4f27ffe9f7cf4c4230adc116dce7a1ecad
Mailen Romero <mailenromero5@gmail.com>
2026-05-10 17:22:12 -0300

    Corregido launch.json

--- .antlr/LogicFormulaLexer.java
+++ .antlr/LogicFormulaLexer.java
@@ -1,4 +1,4 @@
-// Generated from c:/Users/ASUS/ssl-antlr-calculator/LogicFormula.g4 by ANTLR 4.13.1
+// Generated from c:/Users/ASUS/ssl-antlr-53481/LogicFormula.g4 by ANTLR 4.13.1
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
--- .antlr/LogicFormulaParser.java
+++ .antlr/LogicFormulaParser.java
@@ -1,4 +1,4 @@
-// Generated from c:/Users/ASUS/ssl-antlr-calculator/LogicFormula.g4 by ANTLR 4.13.1
+// Generated from c:/Users/ASUS/ssl-antlr-53481/LogicFormula.g4 by ANTLR 4.13.1
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.*;
